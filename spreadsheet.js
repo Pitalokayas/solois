f02ce7e56032949bb8cda41e0525f46960268ae4
Pitalokayas <npitaloka0@gmail.com>
2026-06-22 10:12:46 +0700

    Update spreadsheet.js

--- spreadsheet.js
+++ spreadsheet.js
@@ -1,11 +1,16 @@
-const CSV_URL="https://docs.google.com/spreadsheets/d/e/2PACX-1vRdsw-kqJf1uuvB1Ame5UwemOGitSCmhHGI9r4EpxZOgqXHHEGTbMXFxWT5XE8xzk3MppbC9oa1M0YX/pub?output=csv";
-
-async function loadData(){
-const res=await fetch(CSV_URL);
-const text=await res.text();
-
-return Papa.parse(text,{
-header:true,
-skipEmptyLines:true
-}).data;
-}
+$(document).ready(function() {
+  $('#zossTable').DataTable({
+    ajax: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRdsw-kqJf1uuvB1Ame5UwemOGitSCmhHGI9r4EpxZOgqXHHEGTbMXFxWT5XE8xzk3MppbC9oa1M0YX/pub?output=csv',
+    columns: [
+      { title: "Panjang ZoSS" },
+      { title: "Lebar Jalan" },
+      { title: "Status Jalan" },
+      { title: "Kondisi Marka ZoSS" },
+      { title: "Kondisi Rambu ZoSS" },
+      { title: "Kondisi Zebra Cross" },
+      { title: "Kondisi Pita Penggaduh" },
+      { title: "Volume" }
+    ],
+    paging: true
+  });
+});
